fe5967dc9ac3193adb4ce0cd7ba3d65dbe9dbe8f
zhuhao3 <zhuhao3@xiaomi.com>
2018-12-04 00:24:53 +0800

    leetcode 8

---
+++ src/LeetCode/_8String2Integer.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class _8String2Integer {
+//    public int myAtoi(String str) {
+//        str = str.trim();
+//
+//        if (str.isEmpty()) {
+//            return 0;
+//        }
+//        char[] chs = str.toCharArray();
+//        if (chs[0] == '+'){
+//
+//        } else if (chs[0] == '-') {
+//
+//        } else if (chs[0] >= '0' && chs[0] <= '9') {
+//
+//        } else {
+//            return 0;
+//        }
+//    }
+
+    public static void main(String[] args) {
+        System.out.println((long) Integer.MAX_VALUE + 1);
+    }
+}
