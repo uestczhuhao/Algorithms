cc13e59b126b90217be399f3228bcb677aafb76a
uestczhuhao <uestczhuhao@163.com>
2020-02-01 11:09:15 +0800

    148 list的递归排序，已通过

--- src/LeetCode/_148SortList.java
+++ src/LeetCode/_148SortList.java
@@ -18,20 +18,30 @@ public class _148SortList {
     public static void main(String[] args) {
         ListNode head = new ListNode(4);
 //        ListNode first = new ListNode(5);
-        ListNode first = new ListNode(3);
+        ListNode first = new ListNode(2);
 //        ListNode second = new ListNode(1);
-        ListNode second = new ListNode(2);
+        ListNode second = new ListNode(1);
 //        ListNode third = new ListNode(2);
-//        ListNode third1 = new ListNode(40);
-        ListNode third = new ListNode(1);
+        ListNode third1 = new ListNode(40);
+        ListNode third2 = new ListNode(5);
+        ListNode third = new ListNode(3);
         head.next = first;
         first.next = second;
         second.next = third;
-//        third.next = third1;
+        third.next = third1;
+        third1.next = third2;
         _148SortList t = new _148SortList();
-        t.sortList(head);
+        ListNode newHead = t.sortList(head);
 //        t.merge2List(head,  second, third1);
-        System.out.println();
+        print(newHead);
+//        System.out.println(newHead);
+    }
+
+    private static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
     }
 
     /**
@@ -52,19 +62,41 @@ public class _148SortList {
         }
         for (int step = 1; step < len; step += step) {
             ListNode current = preHead.next;
-            for (int i = 0; i < len - step; i += 2 * step) {
+            ListNode lastRoundLast = null;
+            for (int i = 0; i < len - step && current != null; i += 2 * step) {
                 ListNode leftHead = current;
-                ListNode rightHead = nextKNode(leftHead, step);
-                ListNode rightEnd = nextKNode(rightHead, step);
-                // 左闭右开区间，下一次从rightEnd开始
-                current = rightEnd;
-                //
-                if (i == 0) {
-                    preHead.next = merge2List(leftHead, rightHead, rightEnd);
-                } else {
-                    merge2List(leftHead, rightHead, rightEnd);
+                ListNode leftEnd = nextKNode(leftHead, step - 1);
+                // leftEnd为null，这一轮右边无节点，直接返回
+                if (leftEnd == null) {
+                    break;
                 }
+                ListNode rightHead = leftEnd.next;
+                // 左边断开
+                leftEnd.next = null;
+                ListNode rightEnd = nextKNode(rightHead, step - 1);
+                // current指向下一轮的head节点
+                current = rightEnd == null ? null : rightEnd.next;
+                // 右边断开
+                if (rightEnd != null) {
+                    rightEnd.next = null;
+                }
+
+                ListNode thisRoundHead = merge2List(leftHead, rightHead);
+                if (i == 0) {
+                    preHead.next = thisRoundHead;
+                } else {
+                    lastRoundLast.next = thisRoundHead;
+                }
+                ListNode thisRoundLast = thisRoundHead;
+                while (thisRoundLast.next != null) {
+                    thisRoundLast = thisRoundLast.next;
+                }
+                // 本轮的末节点和下一轮的头结点联合起来
+                thisRoundLast.next = current;
+                // 本轮的末节点赋值给上一轮的末节点
+                lastRoundLast = thisRoundLast;
             }
+
         }
         return preHead.next;
     }
@@ -85,15 +117,13 @@ public class _148SortList {
     }
 
     /**
-     * 合并两个有序链表，区间为左闭右开
-     * TODO : 必须断开后再连起来，否则会发生4->3->2->1 第二次排序后变成 4->3->2 和 1->2，丢元素的情况发生
+     * 合并两个有序链表，必须断开后再连起来，否则会发生4->3->2->1 第二次排序后变成 4->3->2 和 1->2，丢元素的情况发生
      */
-    private ListNode merge2List(ListNode leftHead, ListNode midNode, ListNode rightEnd) {
+    private ListNode merge2List(ListNode leftHead, ListNode rightHead) {
         ListNode newListIndex;
         ListNode newHead = new ListNode(-1);
-        ListNode rightHead = midNode;
         newListIndex = newHead;
-        while (leftHead != midNode && rightHead != rightEnd) {
+        while (leftHead != null && rightHead != null) {
             if (leftHead.val <= rightHead.val) {
                 newListIndex.next = leftHead;
                 leftHead = leftHead.next;
@@ -104,19 +134,17 @@ public class _148SortList {
             newListIndex = newListIndex.next;
         }
 
-        while (leftHead != midNode) {
+        while (leftHead != null) {
             newListIndex.next = leftHead;
             leftHead = leftHead.next;
             newListIndex = newListIndex.next;
         }
 
-        while (rightHead != rightEnd) {
+        while (rightHead != null) {
             newListIndex.next = rightHead;
             rightHead = rightHead.next;
             newListIndex = newListIndex.next;
         }
-        newListIndex.next = rightEnd;
-
         return newHead.next;
     }
 
