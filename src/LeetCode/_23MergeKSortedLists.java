02d7f33bfffe1deae5d4333af8236ff9c28c9b36
uestczhuhao <uestczhuhao@163.com>
2020-02-01 23:12:56 +0800

    非递归版合并

--- src/LeetCode/_23MergeKSortedLists.java
+++ src/LeetCode/_23MergeKSortedLists.java
@@ -4,9 +4,9 @@ import java.util.PriorityQueue;
 
 /**
  * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入:
  * [
  *   1->4->5,
@@ -14,7 +14,6 @@ import java.util.PriorityQueue;
  *   2->6
  * ]
  * 输出: 1->1->2->3->4->4->5->6
- *
  */
 public class _23MergeKSortedLists {
     public static void main(String[] args) {
@@ -26,12 +25,20 @@ public class _23MergeKSortedLists {
         head2.next.next = new ListNode(6);
         ListNode head3 = new ListNode(2);
         head3.next = new ListNode(7);
-        ListNode[] lists = new ListNode[]{head1,head2,head3};
+        ListNode[] lists = new ListNode[]{head1, head2, head3};
 
 //        System.out.println(mergeKLists(lists).val);
-        ListNode res = mergeKListsRecur(lists);
+        ListNode res = mergeKList(lists);
+        print(res);
 //        System.out.println(merge2Lists(head1,head2).val);
         System.out.println();
+    }
+
+    static public void print(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
     }
 
     /**
@@ -39,7 +46,7 @@ public class _23MergeKSortedLists {
      */
     static public ListNode mergeKLists(ListNode[] lists) {
 
-        if (lists == null || lists.length == 0){
+        if (lists == null || lists.length == 0) {
             return null;
         }
 
@@ -52,7 +59,7 @@ public class _23MergeKSortedLists {
             }
         }
         ListNode node = preHead;
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             node.next = new ListNode(queue.poll());
             node = node.next;
         }
@@ -65,37 +72,53 @@ public class _23MergeKSortedLists {
      * 将lists两两合并，直到全部合并成一个
      */
     static public ListNode mergeKListsRecur(ListNode[] lists) {
-        if (lists == null || lists.length == 0){
+        if (lists == null || lists.length == 0) {
             return null;
         }
         return doMergeListRecur(lists, 0, lists.length - 1);
     }
 
-    static ListNode doMergeListRecur(ListNode[] lists, int start, int end){
-        if (start == end){
+    /**
+     * 非递归版的，时间复杂度和递归版的相同
+     */
+    static public ListNode mergeKList(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        int length = lists.length;
+        for (int step = 1; step < length; step += step) {
+            for (int i = 0; i < length - step; i += 2 * step) {
+                lists[i] = merge2Lists(lists[i], lists[i + step]);
+            }
+        }
+        return lists[0];
+    }
+
+    static ListNode doMergeListRecur(ListNode[] lists, int start, int end) {
+        if (start == end) {
             return lists[start];
         }
 
-        int mid = (start + end)/2;
+        int mid = (start + end) / 2;
         ListNode left = doMergeListRecur(lists, start, mid);
-        ListNode right = doMergeListRecur(lists, mid+1, end);
+        ListNode right = doMergeListRecur(lists, mid + 1, end);
 
         return merge2Lists(left, right);
     }
 
-    static ListNode merge2Lists(ListNode node1, ListNode node2){
-        if (node1 == null){
-            return  node2;
+    static ListNode merge2Lists(ListNode node1, ListNode node2) {
+        if (node1 == null) {
+            return node2;
         }
 
-        if (node2 == null){
+        if (node2 == null) {
             return node1;
         }
 
         ListNode preHead = new ListNode(0);
         ListNode node = preHead;
-        while (node1 != null && node2!=null){
-            if (node1.val < node2.val){
+        while (node1 != null && node2 != null) {
+            if (node1.val < node2.val) {
                 node.next = new ListNode(node1.val);
                 node1 = node1.next;
                 node = node.next;
@@ -106,13 +129,13 @@ public class _23MergeKSortedLists {
             }
         }
 
-        while (node1 != null){
+        while (node1 != null) {
             node.next = new ListNode(node1.val);
             node1 = node1.next;
             node = node.next;
         }
 
-        while (node2 != null){
+        while (node2 != null) {
             node.next = new ListNode(node2.val);
             node2 = node2.next;
             node = node.next;
