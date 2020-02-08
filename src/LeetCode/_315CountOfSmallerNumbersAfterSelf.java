51ca2b30c20f86c268439df7b43bba71fd437121
uestczhuhao <uestczhuhao@163.com>
2020-02-08 21:03:02 +0800

    315通过

--- src/LeetCode/_315CountOfSmallerNumbersAfterSelf.java
+++ src/LeetCode/_315CountOfSmallerNumbersAfterSelf.java
@@ -23,9 +23,13 @@ import java.util.stream.IntStream;
 public class _315CountOfSmallerNumbersAfterSelf {
     public static void main(String[] args) {
         _315CountOfSmallerNumbersAfterSelf t = new _315CountOfSmallerNumbersAfterSelf();
-        int[] nums = {4, 3, 1, 2, 0};
+//        int[] nums = {1, 2, 0};
+//        int[] nums = {-1, -1};
+        int[] nums = {26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41};
+//        int[] nums = {5, 2, 6, 1};
         t.countSmaller(nums);
-        System.out.println(Arrays.toString(t.indexArr));
+//        System.out.println(Arrays.toString(t.indexArr));
+        System.out.println(Arrays.toString(t.reverseCount));
     }
 
     /**
@@ -67,17 +71,23 @@ public class _315CountOfSmallerNumbersAfterSelf {
     }
 
     /**
-     * 借助下标数组处理和统计逆序对
-     * @param nums 原数组，用于比较
+     * 借助下标数组处理和统计逆序对，原因是在归并排序中，元素的位置已经改变
+     * 一个元素在算法的执行过程中位置发生变化，我们还想定位它，
+     * 这样的场景我们在 “最小索引堆” 中曾经学习过，从中得到启发，不妨也设置一个 “索引数组” 吧。
+     *
+     * @param nums     原数组，用于比较
      * @param indexArr 下标数组，归并排序实际操作的是下标数组
      */
-    private int doMergeAndCountReverse(int[] nums, int[] indexArr, int low, int mid, int high) {
+    private void doMergeAndCountReverse(int[] nums, int[] indexArr, int low, int mid, int high) {
         System.arraycopy(indexArr, low, assist, low, high - low + 1);
 
         int left = low, right = mid + 1;
         int mergeResult = low;
         while (left <= mid && right <= high) {
-            if (nums[assist[left]] < nums[assist[right]]) {
+            if (nums[assist[left]] <= nums[assist[right]]) {
+                // 每次前置数组出列时，记录数量，数量为后置数组已经出列的个数
+                // 例如前置：[2 5 6 7] 后置：[1 3 4 8]中2出列时，右侧只有1出列，数量为1
+                reverseCount[assist[left]] += right - mid - 1;
                 indexArr[mergeResult++] = assist[left++];
             } else {
                 indexArr[mergeResult++] = assist[right++];
@@ -85,6 +95,10 @@ public class _315CountOfSmallerNumbersAfterSelf {
         }
 
         while (left <= mid) {
+            // 后置数组全出列时，前置数组的每一个都加上右侧数组长度，
+            // 理由是前置中的任意未出列元素比后置所有元素都大
+            // 如：[8 9 10 11] 和 [1 2 3 4]，左侧每个逆序对都是4
+            reverseCount[assist[left]] += high - mid;
             indexArr[mergeResult++] = assist[left++];
         }
 
@@ -92,6 +106,5 @@ public class _315CountOfSmallerNumbersAfterSelf {
             indexArr[mergeResult++] = assist[right++];
         }
 
-        return 0;
     }
 }
