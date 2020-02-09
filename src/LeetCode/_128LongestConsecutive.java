a64f076d8a0f4de8f84483b3376661d2be8760c4
uestczhuhao <uestczhuhao@163.com>
2020-02-09 11:31:36 +0800

    128 注释

--- src/LeetCode/_128LongestConsecutive.java
+++ src/LeetCode/_128LongestConsecutive.java
@@ -3,11 +3,18 @@ package LeetCode;
 import java.util.HashSet;
 import java.util.Set;
 /**
- * Copyright (c) 2018 XiaoMi Inc. All Rights Reserved.
- *
  * @author Zhu Hao <zhuhao3@xiaomi.com>
  * @date 20-1-20 下午7:02.
- * Description:
+ *
+ * 给定一个未排序的整数数组，找出最长连续序列的长度。
+ *
+ * 要求算法的时间复杂度为 O(n)。
+ *
+ * 示例:
+ *
+ * 输入: [100, 4, 200, 1, 3, 2]
+ * 输出: 4
+ * 解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
  */
 public class _128LongestConsecutive {
     public static void main(String[] args) {
@@ -16,15 +23,11 @@ public class _128LongestConsecutive {
     }
 
     /**
-     * 给定一个未排序的整数数组，找出最长连续序列的长度。
+     * 思路：用一个set放置所有元素，从每个连续序列的头开始找，
+     * 判定此序列是否大于当前的最大子序列的长度
      *
-     * 要求算法的时间复杂度为 O(n)。
-     *
-     * 示例:
-     *
-     * 输入: [100, 4, 200, 1, 3, 2]
-     * 输出: 4
-     * 解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+     * 如：有[100, 1, 90, 2, 3, 4]，则从1开始找，循环判定set.contain(num+1)，同时子序列长度+1
+     * 循环退出，从1开始的连续序列长度就确定下来
      */
     static int longestConsecutive(int[] nums) {
         int length = nums.length;
