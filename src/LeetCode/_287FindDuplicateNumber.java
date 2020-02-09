973e078c7d587882b6b45d62878f39e5bad7e813
uestczhuhao <uestczhuhao@163.com>
2020-02-09 23:20:23 +0800

    295 寻找数据流的中位数解法

--- src/LeetCode/_287FindDuplicateNumber.java
+++ src/LeetCode/_287FindDuplicateNumber.java
@@ -3,16 +3,23 @@ package LeetCode;
 /**
  * @author mizhu
  * @date 20-1-19 下午9:23
- * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
- * prove that at least one duplicate number must exist.
+ * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
  *
- * Assume that there is only one duplicate number, find the duplicate one.
+ * 示例 1:
  *
- * note:
- * 1. You must not modify the array (assume the array is read only).
- * 2. You must use only constant, O(1) extra space.
- * 3. Your runtime complexity should be less than O(n2).
- * 4. There is only one duplicate number in the array, but it could be repeated more than once.
+ * 输入: [1,3,4,2,2]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [3,1,3,4,2]
+ * 输出: 3
+ * 说明：
+ *
+ * 不能更改原数组（假设数组是只读的）。
+ * 只能使用额外的 O(1) 的空间。
+ * 时间复杂度小于 O(n2) 。
+ * 数组中只有一个重复的数字，但它可能不止重复出现一次。
+ *
  */
 public class _287FindDuplicateNumber {
     public static void main(String[] args) {
@@ -22,7 +29,7 @@ public class _287FindDuplicateNumber {
 
 
     /**
-     * 数组表示链表，找出链表环的起点，快指针从环起点开始，
+     * 数组表示链表，找出链表环的起点，快指针从环起点开始
      */
     static public int findDuplicate(int[] nums) {
         if (null == nums || nums.length <=0 ) {
