94b9424faf0d2c2c5a55b0d6245ccbd074adf374
uestczhuhao <uestczhuhao@163.com>
2020-02-16 15:41:25 +0800

    注释

--- src/LeetCode/_237DeleteNodeInLinkedList.java
+++ src/LeetCode/_237DeleteNodeInLinkedList.java
@@ -1,41 +1,42 @@
 package LeetCode;
 
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author mizhu
  * @date 20-2-12 下午5:57
  * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
- *
+ * <p>
  * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  *  
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: head = [4,5,1,9], node = 5
  * 输出: [4,1,9]
  * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
  * 示例 2:
- *
+ * <p>
  * 输入: head = [4,5,1,9], node = 1
  * 输出: [4,5,9]
  * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
  *  
- *
+ * <p>
  * 说明:
- *
+ * <p>
  * 链表至少包含两个节点。
  * 链表中所有节点的值都是唯一的。
  * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
  * 不要从你的函数中返回任何结果。
  */
 public class _237DeleteNodeInLinkedList {
-    public static void main(String[] args) {
-
-    }
 
     /**
      * next 的数据赋值给当前值，然后删除next节点
