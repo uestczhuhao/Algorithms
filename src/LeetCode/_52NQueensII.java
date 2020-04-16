a4820f6c6b2babea6517b290a691900d69148983
zhuhao3 <zhuhao3@xiaomi.com>
2020-04-16 17:06:42 +0800

    52 N皇后II

---
+++ src/LeetCode/_52NQueensII.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+/**
+ * Copyright (c) 2018 XiaoMi Inc. All Rights Reserved.
+ *
+ * @author Zhu Hao <zhuhao3@xiaomi.com>
+ * @date 20-4-16 下午4:55.
+ * Description:
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ *
+ *
+ * 上图为 8 皇后问题的一种解法。
+ *
+ * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+ *
+ * 示例:
+ *
+ * 输入: 4
+ * 输出: 2
+ * 解释: 4 皇后问题存在如下两个不同的解法。
+ * [
+ *  [".Q..",  // 解法 1
+ *   "...Q",
+ *   "Q...",
+ *   "..Q."],
+ *
+ *  ["..Q.",  // 解法 2
+ *   "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ *
+ */
+public class _52NQueensII {
+    public int totalNQueens(int n) {
+        return 0;
+    }
+}
