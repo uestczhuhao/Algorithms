6f2fb1e6963695605558345500c9034b455f556d
zhuhao3 <zhuhao3@xiaomi.com>
2020-06-16 16:55:00 +0800

    快慢指针判定循环链表

---
+++ src/LeetCode/_141LinkedListCycle.java
@@ -0,0 +1,77 @@
+package LeetCode;
+
+/**
+ * Copyright (c) 2018 XiaoMi Inc. All Rights Reserved.
+ *
+ * @author Zhu Hao <zhuhao3@xiaomi.com>
+ * @date 20-6-16 下午4:46.
+ * Description:
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ *
+ *  
+ *
+ * 进阶：
+ *
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ *
+ */
+public class _141LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+//        node4.next = node2;
+        _141LinkedListCycle t = new _141LinkedListCycle();
+        System.out.println(t.hasCycle(node1));
+    }
+
+}
