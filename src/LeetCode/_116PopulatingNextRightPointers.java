88ac30308f48fd22e11fe6a920a8bc24b029e45b
zhuhao <uestczhuhao@163.com>
2020-08-15 22:58:10 +0800

    116 填充下一个节点

---
+++ src/LeetCode/_116PopulatingNextRightPointers.java
@@ -0,0 +1,107 @@
+package LeetCode;
+
+/**
+ * @author mizhu
+ * @date 2020/8/15 22:32
+ * <p>
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * <p>
+ * struct Node {
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * <p>
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ * <p>
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ */
+public class _116PopulatingNextRightPointers {
+    /**
+     * 在n层，构建n+1层的next指针，有两种情况
+     * 1. node左孩子的next指针，直接是node的右孩子
+     * 2. node右孩子的指针，是node.next的左孩子
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node head = root;
+        Node levelIndex;
+        // head.next 为空，代表已经到了最后一层，直接返回
+        while (head.left != null) {
+            levelIndex = head;
+            while (levelIndex != null) {
+
+                levelIndex.left.next = levelIndex.right;
+                if (levelIndex.next != null) {
+                    levelIndex.right.next = levelIndex.next.left;
+                }
+                levelIndex = levelIndex.next;
+            }
+            head = head.left;
+        }
+
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root1 = new Node(1);
+        Node root2 = new Node(2);
+        Node root3 = new Node(3);
+        Node root4 = new Node(4);
+        Node root5 = new Node(5);
+        Node root6 = new Node(6);
+        Node root7 = new Node(7);
+        root1.left = root2;
+        root1.right = root3;
+        root2.left = root4;
+        root2.right = root5;
+        root3.left = root6;
+        root3.right = root7;
+
+        _116PopulatingNextRightPointers t = new _116PopulatingNextRightPointers();
+        t.connect(root1);
+    }
+
+    private static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+}
