b000a1689b33ba5c768db2367a0995175934ed1c
zhuhao <uestczhuhao@163.com>
2020-11-14 20:27:31 +0800

    268 丢失的数字

---
+++ src/LeetCode/_268MissingNumber.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+/**
+ * @author mizhu
+ * @date 2020/11/14 20:22
+ */
+public class _268MissingNumber {
+    /**
+     * 从1加到n，再依次减去nums中的值
+     * 最后剩余的即为丢失的数字
+     */
+    public int missingNumber(int[] nums) {
+        if (null == nums || nums.length == 0) {
+            return -1;
+        }
+
+        int sum = nums.length;
+        for (int i=0;i<nums.length;i++){
+            sum+=i;
+            sum -= nums[i];
+        }
+
+        return sum;
+    }
+}
