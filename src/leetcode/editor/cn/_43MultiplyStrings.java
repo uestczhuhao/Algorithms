c5fafb9c263be8057f25d7b0b78659e74edefded
zhuhao <uestczhuhao@163.com>
2021-02-08 15:43:47 +0800

    _43 字符串乘积，竖式优化

--- src/leetcode/editor/cn/_43MultiplyStrings.java
+++ src/leetcode/editor/cn/_43MultiplyStrings.java
@@ -27,8 +27,8 @@ package leetcode.editor.cn;
 public class _43MultiplyStrings {
     public static void main(String[] args) {
         Solution t = new _43MultiplyStrings().new Solution();
-        String num1 = "123";
-        String num2 = "0";
+        String num1 = "9";
+        String num2 = "99";
         System.out.println(t.multiply(num1, num2));
     }
 
@@ -37,10 +37,45 @@ public class _43MultiplyStrings {
      */
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public String multiply(String num1, String num2) {
+            if (num1 == null || "0".equals(num1)) {
+                return "0";
+            }
+
+            if (num2 == null || "0".equals(num2)) {
+                return "0";
+            }
+
+            int len1 = num1.length();
+            int len2 = num2.length();
+            int[] resArr = new int[len1 + len2];
+            for (int i = len1 - 1; i >= 0; i--) {
+                int iThNum = num1.charAt(i) - '0';
+                for (int j = len2 - 1; j >= 0; j--) {
+                    int jThNum = num2.charAt(j) - '0';
+                    int sum = iThNum * jThNum + resArr[i + j + 1];
+                    // 找规律，num1 的第i位和num2 的第j位乘积的结果
+                    // 结果放在 i+j+1位，进位放在高一位，即i+j位
+                    resArr[i + j + 1] = sum % 10;
+                    resArr[i + j] += sum / 10;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < resArr.length; i++) {
+                if (i == 0 && resArr[i] == 0) {
+                    continue;
+                }
+                sb.append(resArr[i]);
+            }
+            return sb.toString();
+        }
+
+
         /**
          * 思路：竖式相乘，最后结果累加
          */
-        public String multiply(String num1, String num2) {
+        public String multiply1(String num1, String num2) {
             if (num1 == null || "0".equals(num1)) {
                 return "0";
             }
