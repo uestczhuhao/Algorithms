2d831d6882868cd7dd747912bece6f71186041bf
zhuhao3 <zhuhao3@xiaomi.com>
2021-02-19 20:22:28 +0800

    _97 动态规划

--- src/leetcode/editor/cn/_97InterleavingString.java
+++ src/leetcode/editor/cn/_97InterleavingString.java
@@ -67,7 +67,7 @@ public class _97InterleavingString {
          * 思路：动态规划，dp[i][j] 表示 s1的前i个元素和s2的前j个元素是否能组成s3的前i+j个元素
          * dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])
          */
-        public boolean isInterleave1(String s1, String s2, String s3) {
+        public boolean isInterleave(String s1, String s2, String s3) {
             if (s1 == null || s1.length() == 0) {
                 return s2.equals(s3);
             }
@@ -86,16 +86,19 @@ public class _97InterleavingString {
             boolean[][] dp = new boolean[len1 + 1][len2 + 1];
             dp[0][0] = true;
 
-            for (int i = 0; i <= len1; i++) {
-                for (int j = 0; j <= len2; j++) {
-                    if (i > 0) {
-                        dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
-                    }
-                    if (j > 0) {
-                        dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
-                    }
+            // 遇到不相等字符后，后面的字符串不可能符合要求
+            for (int i = 1; i <= len1 && s1.charAt(i - 1) == s3.charAt(i - 1); i++) {
+                dp[i][0] = true;
+            }
+
+            // 遇到不相等字符后，后面的字符串不可能符合要求
+            for (int j = 1; j <= len2 && s2.charAt(j - 1) == s3.charAt(j - 1); j++) {
+                dp[0][j] = true;
+            }
+            for (int i = 1; i <= len1; i++) {
+                for (int j = 1; j <= len2; j++) {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
                 }
-                System.out.println(Arrays.toString(dp[i]));
             }
             return dp[len1][len2];
         }
@@ -103,7 +106,7 @@ public class _97InterleavingString {
         /**
          * 空间优化，使用滚动数组
          */
-        public boolean isInterleave(String s1, String s2, String s3) {
+        public boolean isInterleave1(String s1, String s2, String s3) {
             if (s1 == null || s1.length() == 0) {
                 return s2.equals(s3);
             }
