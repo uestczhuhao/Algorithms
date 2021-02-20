cf62a7ef10f9a7e531286a2372f35383696b2471
zhuhao <uestczhuhao@163.com>
2021-02-20 23:14:56 +0800

    _131 拆分回文子串

---
+++ src/leetcode/editor/cn/_131PalindromePartitioning.java
@@ -0,0 +1,116 @@
+package leetcode.editor.cn;
+
+//给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。 
+//
+// 返回 s 所有可能的分割方案。 
+//
+// 示例: 
+//
+// 输入: "aab"
+//输出:
+//[
+//  ["aa","b"],
+//  ["a","a","b"]
+//] 
+// Related Topics 深度优先搜索 动态规划 回溯算法 
+// 👍 486 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _131PalindromePartitioning {
+    public static void main(String[] args) {
+        Solution t = new _131PalindromePartitioning().new Solution();
+        String s = "aab";
+        System.out.println(t.partition(s));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 思路：回溯法，记录&判断每个子串是否是回文串
+         * 注意：判断回文的方法可以提升至O(1)，这样总的时间复杂度能从O(n^3) 提升到 O(n^2)
+         */
+        int len = 0;
+        String str;
+        List<List<String>> answer = new LinkedList<>();
+
+        public List<List<String>> partition1(String s) {
+            if (s == null || s.length() == 0) {
+                return answer;
+            }
+            str = s;
+            len = s.length();
+            dfs(0, new LinkedList<>());
+            return answer;
+        }
+
+        private void dfs(int start, Deque<String> path) {
+            if (start == len) {
+                answer.add(new ArrayList<>(path));
+                return;
+            }
+
+            for (int i = start; i < len; i++) {
+                if (isPalindrome(start, i)) {
+                    path.add(str.substring(start, i + 1));
+                    dfs(i + 1, path);
+                    path.removeLast();
+                }
+            }
+        }
+
+        private boolean isPalindrome(int start, int end) {
+            return dp[start][end];
+            // O(n) 的解法
+//            if (start == end) {
+//                return true;
+//            }
+//
+//            while (start < end) {
+//                if (str.charAt(start) != str.charAt(end)) {
+//                    return false;
+//                }
+//                start++;
+//                end--;
+//            }
+
+//            return true;
+        }
+
+        /**
+         * 思路：借助动态规划求回文字符串的思想
+         * 预先求出s的子串是否为回文子串，其中dp[i][j]表示字符串的i～j之间的子串是否为回文
+         * dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j]
+         */
+        boolean[][] dp;
+
+        public List<List<String>> partition(String s) {
+            if (s == null || s.length() == 0) {
+                return answer;
+            }
+            str = s;
+            len = s.length();
+            dp = new boolean[len + 1][len + 1];
+            for (int right = 0; right <= len - 1; right++) {
+                for (int left = 0; left <= right; left++) {
+                    // right - left <= 2 表示三种情况
+                    // left和right二者中间只有一个元素；没有元素；边界重合，三种情况统一处理
+                    if (str.charAt(left) == str.charAt(right) && (right - left <= 2 || dp[left + 1][right - 1])) {
+                        dp[left][right] = true;
+                    }
+                }
+            }
+
+            dfs(0, new LinkedList<>());
+
+            // 存放结果，dp[i][j] 表示从i到j的字符串是否为回文子串
+            return answer;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/_5LongestPalindromicSubstring.java
@@ -0,0 +1,90 @@
+package leetcode.editor.cn;
+
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "a"
+//输出："a"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "ac"
+//输出："a"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母（大写和/或小写）组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 3215 👎 0
+
+
+public class _5LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution t = new _5LongestPalindromicSubstring().new Solution();
+        String s = "babad";
+        System.out.println(t.longestPalindrome(s));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 动态规划解法
+         * dp[i][j] 代表i到j的字符串是否为回文
+         * 转移方程为：dp[i][j] = dp[i+1][j-1] && s[i+1] == s[j-1]
+         */
+        public  String longestPalindrome(String s) {
+            if (null == s || s.length() == 0) {
+                return "";
+            }
+
+            int len = s.length();
+            boolean[][] dp = new boolean[len][len];
+
+            int low = 0, high = 0;
+            for (int right = 0; right < len; right++) {
+                for (int left = 0; left <= right; left++) {
+                    // 如果left和right 相邻，则判断二者字符是否相等
+                    // 否则范围缩小2再判断
+                    if (s.charAt(left) == s.charAt(right) && (right - left <= 2 || dp[left + 1][right - 1])) {
+                        dp[left][right] = true;
+                    if (right - left > high - low) {
+                        low = left;
+                        high = right;
+                    }
+                    }
+
+                }
+            }
+            return s.substring(low, high + 1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
