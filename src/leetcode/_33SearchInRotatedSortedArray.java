bb4c621120571f80733c08ef2f8ff59eacb0a196
zhuhao <uestczhuhao@163.com>
2021-03-09 14:42:38 +0800

    _33 二分法找旋转有序数组中的目标值

--- src/leetcode/_33SearchInRotatedSortedArray.java
+++ src/leetcode/_33SearchInRotatedSortedArray.java
@@ -6,8 +6,10 @@ package leetcode;
 public class _33SearchInRotatedSortedArray {
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 0, 1, 2, 3};
-        System.out.println(findHeadIndex1(nums));
-        System.out.println(search(nums, 3));
+//        System.out.println(findHeadIndex1(nums));
+//        System.out.println(search(nums, 3));
+        _33SearchInRotatedSortedArray t = new _33SearchInRotatedSortedArray();
+        System.out.println(t.search1(nums, 3));
     }
 
     public static int search(int[] nums, int target) {
@@ -24,6 +26,54 @@ public class _33SearchInRotatedSortedArray {
         } else {
             return findTarget(nums, index, nums.length - 1, target);
         }
+    }
+
+    /**
+     * 思路：https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/ji-bai-liao-9983de-javayong-hu-by-reedfan/
+     * 分两种情况
+     * 1 nums[low] <= nums[mid] 时，前半部分有序
+     *  a 如果nums[low] <= target < nums[mid]，去前面一半找
+     *  b 否则，去后面一半找
+     * 2 nums[low] > nums[mid]否则后半部分有序
+     *  a 如果nums[mid] < target <= nums[high]，则去后面一半找
+     *  b 否则，去前面一半找
+     */
+    public int search1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int low =0, high = nums.length -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+
+            // 前面一半有序
+            if (nums[low] <= nums[mid]) {
+                // 前一半有序且target在前面一半
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    // target在后面一半，且后一半无序
+                    low = mid + 1;
+                }
+            } else { // 后面一半有序
+                // 后面一半有序且target在后面一半
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    // target在前面一半，且前一半无序
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 
     private static int findHeadIndex(int[] nums) {
@@ -59,14 +109,12 @@ public class _33SearchInRotatedSortedArray {
     private static int findTarget(int[] nums, int low, int high, int target) {
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (target == nums[mid]) {
-                return mid;
-            }
-
             if (target > nums[mid]) {
                 low = mid + 1;
-            } else {
+            } else if (target < nums[mid]){
                 high = mid - 1;
+            } else {
+                return mid;
             }
         }
         return -1;
