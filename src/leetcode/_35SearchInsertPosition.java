f7310392352ed220dedfed80ccea1e3ff91f6744
zhuhao <uestczhuhao@163.com>
2021-03-10 13:58:23 +0800

    _35 简单题，二分法求插入位置

--- src/leetcode/_35SearchInsertPosition.java
+++ src/leetcode/_35SearchInsertPosition.java
@@ -6,20 +6,13 @@ package leetcode;
 public class _35SearchInsertPosition {
     public static void main(String[] args) {
         int[] nums = {1,3,5,9};
-        System.out.println(searchInsert(nums,6));
+        System.out.println(searchInsert(nums,0));
     }
     public static int searchInsert(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
         int low = 0, high = nums.length - 1;
-
-        if (target < nums[low]) {
-            return low;
-        }
-        if (target > nums[high]) {
-            return high+1;
-        }
 
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -34,6 +27,7 @@ public class _35SearchInsertPosition {
             }
         }
 
+        // 经过二分法（low <= high的方式），low刚好指向下一个大于（没找到）或等于target（找到了）的位置，right指向low的左边位置
         return low ;
     }
 }
