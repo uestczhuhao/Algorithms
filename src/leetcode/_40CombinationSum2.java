dbba883967a725c12214004f5ef92083c5d2ed42
zhuhao <uestczhuhao@163.com>
2021-03-10 23:15:35 +0800

    _40回溯法解组合2

--- src/leetcode/_40CombinationSum2.java
+++ src/leetcode/_40CombinationSum2.java
@@ -8,9 +8,52 @@ import java.util.*;
 public class _40CombinationSum2 {
 
     public static void main(String[] args) {
-        int[] candidates = {1,1,1,2,2};
-        System.out.println(combinationSum2(candidates, 4));
+        int[] candidates = {1, 1, 1, 2, 2};
+        int target = 4;
+        System.out.println(combinationSum2(candidates, target));
+        _40CombinationSum2 t = new _40CombinationSum2();
+        System.out.println(t.combinationSum21(candidates, target));
     }
+
+    List<List<Integer>> ans = new LinkedList<>();
+    Deque<Integer> path = new LinkedList<>();
+
+    /**
+     * 回溯+剪枝
+     * for循环横向遍历，递归纵向遍历
+     * 1 横向遍历时剪枝，nums[i] == nums[i-1]时，跳过
+     * 2 每个数字取一次，因此下一次递归从i + 1 开始
+     * 3 跳过不可能的组合，即target < candidate[i]的分支
+     */
+    public List<List<Integer>> combinationSum21(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) {
+            return ans;
+        }
+
+        Arrays.sort(candidates);
+        doCombination1(0, candidates, target);
+        return ans;
+    }
+
+    private void doCombination1(int startIndex, int[] candidates, int target) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.length && candidates[i] <= target; i++) {
+            // i > startIndex && candidates[i] == candidates[i - 1]
+            // 用于去除本层的重复元素，因为for循环的所有i都处于同一层，相同的元素只要第一个参与运算就够了
+            if (i > startIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            path.addLast(candidates[i]);
+            doCombination1(i + 1, candidates, target - candidates[i]);
+            path.removeLast();
+        }
+    }
+
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (candidates == null || candidates.length == 0) {
@@ -27,7 +70,7 @@ public class _40CombinationSum2 {
     }
 
     public static void doCombination(int start, int[] candidates, Set<List<Integer>> res,
-                              int target, List<Integer> curList) {
+                                     int target, List<Integer> curList) {
         if (target == 0) {
             res.add(new ArrayList<>(curList));
         } else if (target < 0) {
@@ -45,7 +88,7 @@ public class _40CombinationSum2 {
                 [1, 1, 2, 2] from index [0, 2, 3, 4] of the candidates;
                 [1, 1, 2, 2] from index [1, 2, 3, 4] of the candidates.
                  */
-                if (i > start && candidates[i] == candidates[i-1]){
+                if (i > start && candidates[i] == candidates[i - 1]) {
                     continue;
                 }
                 curList.add(candidates[i]);
