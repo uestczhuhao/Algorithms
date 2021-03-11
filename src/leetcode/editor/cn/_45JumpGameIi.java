66789e6753a933c537b50a51dc11379e51ef4cc4
zhuhao <uestczhuhao@163.com>
2021-03-11 10:32:45 +0800

    _45跳跃游戏II加注释

--- src/leetcode/editor/cn/_45JumpGameIi.java
+++ src/leetcode/editor/cn/_45JumpGameIi.java
@@ -24,6 +24,8 @@ package leetcode.editor.cn;
 public class _45JumpGameIi {
     public static void main(String[] args) {
         Solution t = new _45JumpGameIi().new Solution();
+        int[] nums = {1, 3, 1};
+        System.out.println(t.jump(nums));
     }
 
     /**
@@ -33,27 +35,28 @@ public class _45JumpGameIi {
     class Solution {
         /**
          * 思路：贪心算法，从位置0出发，每次跳最大距离，判断最后是否能到最后
-         *
-         * @param nums
-         * @return
+         * 选len-2而不是len-1的原因是，若选len-1为最后一个i的位置
+         * 则每次i == curFurthest 时，要判断 curFurthest== len-1是否成立，成立则步数不+1，不成立+1
+         * <p>
+         * 注意：题设为一定能跳到最后一个位置，因此能到达len-2位置就一定能到最后一个位置
          */
         public int jump(int[] nums) {
             if (nums == null || nums.length <= 1) {
                 return 0;
             }
-            int step = 1;
+            int step = 0;
             // 代表当前位置i所能跳到的最远位置
-            // 初始化：第一跳的边界为nums[0]
-            int boundary = nums[0];
-            int furthest = 0;
+            int curFurthest = 0;
+            int nextFurthest = 0;
             // 注意不需要访问最后一个元素，因为在最后一个元素之前是最后一跳
-            for (int i = 1; i < nums.length - 1; i++) {
+            for (int i = 0; i < nums.length - 1; i++) {
                 // 计算在边界内，能跳到的最远距离
-                furthest = Math.max(furthest, i+ nums[i]);
+                nextFurthest = Math.max(nextFurthest, i + nums[i]);
                 // 代表此时处于边界，必须进行下一跳
-                if (i == boundary) {
-                    boundary = furthest;
-                    step ++;
+                // 如果当前的边界boundary大于len-2，则i达不到这里，步数不需要+1，否则需要+1
+                if (i == curFurthest) {
+                    curFurthest = nextFurthest;
+                    step++;
                 }
             }
 
