f0a70b07ee101aa481d064e4a9b2dde590993fc8
zhuhao <uestczhuhao@163.com>
2021-03-13 23:04:38 +0800

    _90 子集问题

--- src/leetcode/editor/cn/_90SubsetsIi.java
+++ src/leetcode/editor/cn/_90SubsetsIi.java
@@ -49,6 +49,11 @@ public class _90SubsetsIi {
             return answer;
         }
 
+        /**
+         * 回溯法，每次进入回溯时搜集结果
+         * 注意要去重：先排序，再做跳过相同元素的操作，在同一层（for循环）做去重
+         * 当i不是本层的第一个且i和i-1相同，则代表nums[i]这个值已经在i-1处被取过了，直接跳过即可
+         */
         private void dfs(int start, LinkedList<Integer> curSubset) {
             answer.add(new ArrayList<>(curSubset));
             for (int i = start; i < numArr.length; i++) {
