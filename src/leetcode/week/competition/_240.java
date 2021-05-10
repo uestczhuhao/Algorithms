f7a1b40abba5c559a85127c1df8a9d0257a91390
zhuhao3 <zhuhao3@xiaomi.com>
2021-05-10 11:32:42 +0800

    240周赛，有向图中最大颜色

--- src/leetcode/week/competition/_240.java
+++ src/leetcode/week/competition/_240.java
@@ -1,12 +1,8 @@
 package leetcode.week.competition;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author mizhu
@@ -21,6 +17,9 @@ public class _240 {
 //        System.out.println(t.maxDistance(nums1, nums2));
         int[] nums = {3, 1, 5, 6, 4, 2};
         System.out.println(t.maxSumMinProduct(nums));
+        String colors = "abaca";
+        int[][] e = {{0,1},{0,2},{2,3},{3,4}};
+        System.out.println(t.largestPathValue(colors, e));
     }
 
     public int maximumPopulation(int[][] logs) {
@@ -105,37 +104,77 @@ public class _240 {
     }
 
     public int largestPathValue(String colors, int[][] edges) {
-        HashMap<Integer, Set<Integer>> downStream = new HashMap<>(edges.length);
-        HashMap<Integer, Set<Integer>> upStream = new HashMap<>(edges.length);
+        int len = colors.length();
+        // 邻接表，每个位置i放置与i直接相连的下游节点
+        Node[] adjList = new Node[len];
+        // 节点入度数组
+        int[] inDegree = new int[len];
         for (int[] eg : edges) {
             int src = eg[0];
             int tgt = eg[1];
-            Set<Integer> downs = downStream.getOrDefault(src, new HashSet<>());
-            downs.add(tgt);
-            downStream.put(src, downs);
-            Set<Integer> ups = upStream.getOrDefault(tgt, new HashSet<>());
-            ups.add(src);
-            upStream.put(tgt, ups);
+            /*  「头插法」等价于下面几行操作,
+             *    NewNode node = new NewNode(to);
+             *    node.next = adjList[from];
+             *    adjList[from] = node;
+             */
+            adjList[src] = new Node(tgt, adjList[src]);
+            inDegree[tgt]++;
         }
 
-        Set<Integer> allDowns = downStream.values().stream().flatMap(Collection::stream).collect(Collectors.toSet());
-        Set<Integer> allUps = upStream.values().stream().flatMap(Collection::stream).collect(Collectors.toSet());
-        allDowns.retainAll(allUps);
-        Set<Integer> zeroIn = allDowns;
-
-        int max = 0;
-        int[] colorNum;
-        for (int root : zeroIn) {
-            colorNum = new int[26];
-            int curNum = computeMaxColor(downStream, root, colorNum, colors);
-            max = Math.max(curNum, max);
+        // 拓扑排序，记录遍历节点个数
+        int found = 0;
+        // 存放入度为0的节点
+        Deque<Integer> deque = new LinkedList<>();
+        // 截止到第 i 个节点，第 j 种颜色的数量
+        int[][] f = new int[len][26];
+        for (int i = 0; i < len; i++) {
+            if (inDegree[i] == 0) {
+                deque.offer(i);
+            }
         }
 
-        return max;
+        // 拓扑排序
+        while (!deque.isEmpty()) {
+            int cur = deque.poll();
+            found++;
+            // 当前位置的颜色+1
+            f[cur][colors.charAt(cur) - 'a']++;
+
+            // 遍历当前节点的所有下一个节点
+            for (Node next = adjList[cur]; next != null; next = next.next) {
+                inDegree[next.val]--;
+
+                // next节点入度为0时，加入队列
+                if (inDegree[next.val] == 0) {
+                    deque.offer(next.val);
+                }
+
+                // 更新 next 所有颜色数量，即f[next][c] = Math.max(f[cur][c], f[next][c])
+                for (int c = 0; c < 26; c++) {
+                    f[next.val][c] = Math.max(f[next.val][c], f[cur][c]);
+                }
+            }
+        }
+
+        if (found != len) {
+            return -1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            ans = Math.max(ans, Arrays.stream(f[i]).max().getAsInt());
+        }
+        return ans;
     }
 
-    private int computeMaxColor(HashMap<Integer, Set<Integer>> downStream, int node, int[] colorNum, String colorStr) {
-        char ch;
-        return 0;
+    static class Node {
+        int val;
+        Node next;
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
 }
