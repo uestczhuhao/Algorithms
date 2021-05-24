9b72075c928d9cf25392fa7707587b5d0a17e8df
zhuhao <uestczhuhao@163.com>
2021-05-24 19:37:43 +0800

    241 周赛

--- src/leetcode/week/competition/_241.java
+++ src/leetcode/week/competition/_241.java
@@ -1,5 +1,8 @@
 package leetcode.week.competition;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author mizhu
  * @date 2021/5/16 10:31
@@ -12,11 +15,57 @@ public class _241 {
 //        System.out.println(t.minSwaps("100"));
 //        System.out.println(t.minSwaps("1110"));
 //        System.out.println(t.minSwaps("0010111"));
-        System.out.println(t.minSwaps("00011110110110000000000110110101011101111011111101010010010000000000000001101101010010001011110000001101111111110000110101101101001011000011111011101101100110011111110001100110001110000000001100010111110100111001001111100001000110101111010011001"));
+//        System.out.println(t.minSwaps("00011110110110000000000110110101011101111011111101010010010000000000000001101101010010001011110000001101111111110000110101101101001011000011111011101101100110011111110001100110001110000000001100010111110100111001001111100001000110101111010011001"));
     }
 
 
+    //TODO：剩余1题待研究
+
+    /**
+     * 思路：
+     * 1 分摊时间复杂度，初始化时间复杂度为线性，避免O（n^2）
+     * 2 把数组长度较长的num2储存起来，便于在O（1）的时间获取结果
+     */
+    class FindSumPairs {
+        private int[] nums1;
+        private int[] nums2;
+        private Map<Integer, Integer> num2Map = new HashMap<>();
+
+        public FindSumPairs(int[] nums1, int[] nums2) {
+            this.nums1 = nums1;
+            this.nums2 = nums2;
+            buildSumCountMap();
+        }
+
+        private void buildSumCountMap() {
+            for (int j = 0; j < nums2.length; j++) {
+                num2Map.put(nums2[j], num2Map.getOrDefault(nums2[j], 0) + 1);
+            }
+        }
+
+        public void add(int index, int val) {
+            num2Map.put(nums2[index], num2Map.getOrDefault(nums2[index], 0 ) - 1);
+            nums2[index] += val;
+            num2Map.put(nums2[index], num2Map.getOrDefault(nums2[index], 0 ) + 1);
+        }
+
+        /**
+         * 把较长的num2放在map中，降低时间复杂度
+         */
+        public int count(int tot) {
+            int ans = 0;
+            for (int num : nums1) {
+                if (num2Map.containsKey(tot - num)) {
+                    ans += num2Map.get(tot - num);
+                }
+            }
+
+            return ans;
+        }
+    }
+
     int answer = 0;
+
     public int subsetXORSum(int[] nums) {
         if (nums.length == 0) {
             return answer;
@@ -77,21 +126,5 @@ public class _241 {
         return dif / 2;
     }
 
-
-    //TODO：剩余两题待研究
-//    class FindSumPairs {
-//
-//        public FindSumPairs(int[] nums1, int[] nums2) {
-//
-//        }
-//
-//        public void add(int index, int val) {
-//
-//        }
-//
-//        public int count(int tot) {
-//
-//        }
-//    }
 
 }
