1f2d9737ce581cd33768c28823c033baf5afaa59
zhuhao3 <zhuhao3@xiaomi.com>
2021-07-06 16:31:14 +0800

    k个一组翻转链表

---
+++ src/leetcode/editor/cn/_25ReverseNodesInKGroup.java
@@ -0,0 +1,126 @@
+package leetcode.editor.cn;
+
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 
+//
+// k 是一个正整数，它的值小于或等于链表的长度。 
+//
+// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+//
+// 进阶： 
+//
+// 
+// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？ 
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 1
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：head = [1], k = 1
+//输出：[1]
+// 
+//
+// 
+// 
+//
+// 提示： 
+//
+// 
+// 列表中节点的数量在范围 sz 内 
+// 1 <= sz <= 5000 
+// 0 <= Node.val <= 1000 
+// 1 <= k <= sz 
+// 
+// Related Topics 递归 链表 
+// 👍 1160 👎 0
+
+
+public class _25ReverseNodesInKGroup {
+    public static void main(String[] args) {
+        Solution t = new _25ReverseNodesInKGroup().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * k个一组翻转链表
+         */
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+
+            ListNode pre = dummy;
+            ListNode end = dummy;
+
+            while (end.next != null) {
+                for (int i = 0; i < k && end != null; i++) {
+                    end = end.next;
+                }
+
+                if (end == null) {
+                    break;
+                }
+
+                ListNode start = pre.next;
+                ListNode next = end.next;
+                end.next = null;
+                pre.next = reverse(start);
+                start.next = next;
+
+                pre = start;
+                end = pre;
+            }
+
+            return dummy.next;
+        }
+
+        private ListNode reverse(ListNode head) {
+            ListNode pre = null;
+            ListNode cur = head;
+            while (cur != null) {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
